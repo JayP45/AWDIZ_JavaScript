388b69de33439c5554e2751240c5115c7de32dae
Jay Patil <122766420+JayP45@users.noreply.github.com>
2023-10-27 09:27:12 +0530

    JS Day1

--- Day1/day1.js
+++ Day1/day1.js
@@ -1,1 +1,6 @@
-console.log(5+6)
+console.log('hello world') // string datatype
+console.log("hello world")
+console.log(`hello world`)
+console.log(typeof 'hello world')
+console.log("1" + 3)  //concatinates string and number result 13
+console.log(1 + 3) // addtitoin is perfome. Number datatype
